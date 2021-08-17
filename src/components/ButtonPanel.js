1e8208b90bf6263b9f0f4470811bd1d85bdb95d1
Omar Labana <omarlabana@outlook.com>
2021-08-17 17:32:03 +0200

    Add map functions to render all buttons

--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -1,8 +1,81 @@
+import React from 'react';
+import Button from './Button';
 function ButtonPanel() {
-    return (
-        <React.Fragment>
+    const buttons = [
+        [
+            {
+                text: "AC"
+            },
+            {
+                text: "+/-"
+            },
+            {
+                text: "%"
+            },
+            {
+                text: "÷"
+            },
+        ],
+        [
+            {
+                text: "2"
+            },
+            {
+                text: "7"
+            },
+            {
+                text: "8"
+            },
+            {
+                text: "X"
+            },
+        ],
+        [
+            {
+                text: "4"
+            },
+            {
+                text: "5"
+            },
+            {
+                text: "6"
+            },
+            {
+                text: "-"
+            },
+        ],
+        [
+            {
+                text: "1"
+            },
+            {
+                text: "2"
+            },
+            {
+                text: "3"
+            },
+            {
+                text: "+"
+            },
+        ],
+        [
+            {
+                text: "0"
+            },
+            {
+                text: "."
+            },
+            {
+                text: "="
+            },
+        ],
+    ]
+    const buttonItems = buttons.map((array) => <div>{array.map((item) => <Button buttonName={item.text} key={item.text} />)}</div>)
 
-        </React.Fragment>
+    return (
+        <>
+            {buttonItems}
+        </>
     );
 }
 

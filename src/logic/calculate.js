65bad1d9a486163edded47f70821a29125816897
Omar Labana <omarlabana@outlook.com>
2021-08-18 22:04:30 +0200

    Edit calculate module

--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -20,21 +20,36 @@ const calculate = (actionData, buttonName) => {
     case '.':
       total = `${total}.`;
       break;
-    case '=':
-      if (!total || !next || !operationName) return 0;
-      total = (total + operationName + next).toString();
-      break;
     case '+': case '-': case '*': case '/': case '%':
+      operationName = buttonName;
+      next = '';
+      break;
+    case '=':
       total = operate(total, next, operationName);
       break;
     case '0': case '1': case '2': case '3': case '4': case '5': case '6': case '7': case '8': case '9':
-      if (!total) total = buttonName;
-      if (total && !next) next = buttonName;
+      next = next ? next += buttonName : buttonName;
+      total = operationName ? total : next;
       break;
     default:
       total = 'Syntax Error';
   }
-  return actionData;
+
+  if (total) {
+    total = total.toString();
+  }
+  if (next) {
+    next = next.toString();
+  }
+  if (operationName) {
+    operationName = operationName.toString();
+  }
+
+  return {
+    total,
+    next,
+    operationName,
+  };
 };
 
 export default calculate;

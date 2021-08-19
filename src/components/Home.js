a2384397a3876ee876c083ae933d0c1d91622542
Omar Labana <omarlabana@outlook.com>
2021-08-20 01:46:53 +0200

    Add other views

---
+++ src/components/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Home = () => (
+  <main>
+    <h2 className="text-2xl mt-2">Welcome to our page!</h2>
+    <p className="text-2xl mt-2">
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+    </p>
+    <p className="text-2xl mt-2">
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+    </p>
+  </main>
+);
+
+export default Home;
